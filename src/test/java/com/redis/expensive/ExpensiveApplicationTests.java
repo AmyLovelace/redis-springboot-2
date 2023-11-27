b6cf1f2bd36b520b94ace89cbd295cde1d3f13b6
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-26 22:25:37 -0300

    first commit

---
+++ src/test/java/com/redis/expensive/ExpensiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis.expensive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
